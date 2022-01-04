70665dd84ecac7c2748d8c9e251115abc12537ef
ceszkraft <cesz_@live.com>
2022-01-04 18:31:38 -0300

    Projeto "finalizado"

---
+++ js/javaScript.js
@@ -0,0 +1,21 @@
+console.log('O intuito desse projeto é apenas usar o bootstrap. O Javascript fica pra um futuro próximo. :)')
+
+btn = document.querySelectorAll('a.btn')
+btn[0].onclick = function() {
+    alert('Manutenção')
+}
+btn[1].onclick = function() {
+    alert('Manutenção')
+}
+btn[2].onclick = function() {
+    alert('Manutenção')
+}
+btn[3].onclick = function() {
+    alert('Manutenção')
+}
+btn[4].onclick = function() {
+    alert('Manutenção')
+}
+btn[5].onclick = function() {
+    alert('Manutenção')
+}
